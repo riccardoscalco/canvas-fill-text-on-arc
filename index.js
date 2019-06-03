57e9daa9737250c6a4a0187d8222d761916dc33a
Riccardo Scalco <riccardo.scalco@gmail.com>
2019-06-03 16:12:49 +0200

    add xo as linter

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 export default fillTextOnArc;
 
 function fillTextOnArc(ctx, text, cx, cy, r, spacing) {
-	for(var i = 0; i < text.length; i++) {
-		const alpha = 2 * Math.PI * i * spacing;
-		fillCharOnArc(ctx, text[i], cx, cy, r, alpha)
+	for (let i = 0; i < text.length; i++) {
+		const alpha = (2 * Math.PI) * (i * spacing);
+		fillCharOnArc(ctx, text[i], cx, cy, r, alpha);
 	}
 }
 
 function fillCharOnArc(ctx, char, cx, cy, r, alpha) {
-	const x = cx + r * Math.cos(alpha);
-	const y = cy + r * Math.sin(alpha);
+	const x = cx + (r * Math.cos(alpha));
+	const y = cy + (r * Math.sin(alpha));
 	ctx.save();
-	ctx.translate(x,y);
-	ctx.rotate(alpha + Math.PI / 2);
+	ctx.translate(x, y);
+	ctx.rotate(alpha + (Math.PI / 2));
 	ctx.fillText(char, 0, 0);
 	ctx.restore();
 }
